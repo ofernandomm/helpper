e626a0fbb9b4d0532c8be415577845c0e9fd7b64
Omar <ofernandomm@gmail.com>
2022-11-25 15:00:58 -0500

    agregando fuentes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import { ActivityIndicator,View,StyleSheet} from 'react-native';
 import {Categories} from './screens'
+import{useFonts} from 'expo-font'
+
+
 
 export default function App() {
+  const [loaded] = useFonts({
+    'Lato-Regular' : require('../assets/fonts/Lato-Regular.ttf'),
+    'Lato-Bold': require('../assets/fonts/Lato-Bold.ttf')
+  })
+
+  if (!loaded) {
+    return (
+      <View styles={styles.container}>
+        <ActivityIndicator color="#DAC4F7" size="large" />
+      </View>
+    );
+  }
+
   return (
     <Categories></Categories>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
--- src/screens/categories/styles.js
+++ src/screens/categories/styles.js
@@ -5,5 +5,9 @@ export const styles = StyleSheet.create({
         flex:1,
         justifyContent:'center',
         alignItems:'center'
-    } 
+    },
+    title:{
+        fontFamily:'Lato-Bold',
+        fontSize:20
+    }
 })
